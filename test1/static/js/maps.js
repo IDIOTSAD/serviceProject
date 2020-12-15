b1ea8a88f0170b2d6ae44f0ca52c7ba482483022
IDIOTSAD <55529455+IDIOTSAD@users.noreply.github.com>
2020-12-15 15:37:01 +0900

    no message

--- test1/static/js/maps.js
+++ test1/static/js/maps.js
@@ -1,7 +1,7 @@
 var map;
 
 function initMap() {
-  var seoul = { lat: 37.5642135 ,lng: 127.0016985 };
+  var seoul = { lat: 37.0 ,lng: 128.0016985 };
   map = new google.maps.Map( document.getElementById('map'), {
       zoom: 12,
       center: seoul
